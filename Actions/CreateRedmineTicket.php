eb1d7ecc96171b0be7c6d8e490db6843173cce85
Andrej Kabachnik <andrej_ka@gmx.de>
2017-06-02 16:14:50 +0200

    NEW history entries on test fails + model improvements

--- Actions/CreateRedmineTicket.php
+++ Actions/CreateRedmineTicket.php
@@ -3,6 +3,7 @@ namespace axenox\TestMan\Actions;
 
 use exface\Core\Actions\CreateData;
 use exface\Core\Exceptions\Actions\ActionInputInvalidObjectError;
+use exface\Core\Factories\DataSheetFactory;
 
 /**
  * This action switches on the record mode in the ActionTest context
@@ -28,36 +29,9 @@ class CreateRedmineTicket extends CreateData
             throw new ActionInputInvalidObjectError($this, 'Only TestMan tickets are accepted as input for "' . $this->getAliasWithNamespace() . '": "' . $input->getMetaObject()->getAliasWithNamespace() . '" given instead!', '6T5DMUS');
         }
         
-        if ($exception = $input->getCellValue('EXCEPTION', 0)) {
-            $exception = '
-					+*Exception:*+
-					<pre style="height: 150px; overflow: scroll;">' . $exception . '</pre>';
-        }
-        
-        if ($soll = $input->getCellValue('SOLL', 0)) {
-            $soll = '+*SOLL:*+
-					' . $soll;
-        }
-        
-        $today = date("d.m.Y");
-        
-        $description = <<<TEXT
-+*Testnotiz:*+ 
-*AID:* {$input->getCellValue('TEST_CASE__FEATURE__MODULE__SHORT_NAME', 0)} {$input->getCellValue('TEST_CASE__FEATURE__MODULE__NAME', 0)}
-*Dialog/Menü:* {$input->getCellValue('TEST_CASE__FEATURE__MENU', 0)}
-*Testsystem:* {$input->getCellValue('SYSTEM', 0)}alexa, Klient {$input->getCellValue('MANDANT', 0)}, getestet in Version:
-{$today} {$input->getCellValue('VERSION', 0)}
-*DB-Nutzer/Schema:* {$input->getCellValue('DBSCHEMA', 0)}
-		
-+*Testablauf:*+ 
-{$input->getCellValue('TESTABLAUF', 0)}
-
-{$exception}
-
-{$soll}
-TEXT;
+        $description = $this->buildTicketBody(null, $input);
         $ticket_object = $this->getWorkbench()->model()->getObject('axenox.RedmineConnector.ISSUE');
-        $result = $this->getWorkbench()->data()->createDataSheet($ticket_object);
+        $result = DataSheetFactory::createFromObject($ticket_object);
         // First add all direct ticket attributes
         foreach ($input->getColumns() as $col) {
             if (strpos($col->getName(), 'REDMINE_TICKET__') === 0) {
@@ -74,31 +48,58 @@ TEXT;
         }
         
         $this->getWorkbench()->context()->getScopeWindow()->getContext('Upload')->clearUploads();
-        $new_rows = $result->dataCreate();
-        
-        if ($new_rows > 0) {
-            // The following is a workaround for a strange bug, that returns an Error 500 upon creating a ticket instead of a ticket number.
-            // The idea is simply to fetch the most recent ticket of the current user.
-            // IDEA This workaround should be removed once the redmine bug is fixed
-            $check = $this->getWorkbench()->data()->createDataSheet($ticket_object);
-            $check->addFilterFromString('AUTHOR', $ticket_object->getDataConnection()->getUserId());
-            $check->getColumns()->addFromExpression($ticket_object->getUidAlias());
-            $check->dataRead();
-            $new_ticket_id = $check->getCellValue($ticket_object->getUidAlias(), 0);
-        } else {
-            $new_ticket_id = $result->getUidColumn()->getCellValue(0);
-        }
+        $result->dataCreate();
+        $new_ticket_id = $result->getUidColumn()->getCellValue(0);
         
         // Now save a reference for the newly created ticket for the test case
-        $ticket_ref = $this->getWorkbench()->data()->createDataSheet($this->getWorkbench()->model()->getObject('axenox.TestMan.TICKET'));
+        $ticket_ref = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.TestMan.TICKET');
         $ticket_ref->setCellValue('TEST_CASE', 0, $input->getCellValue('TEST_CASE', 0));
         $ticket_ref->setCellValue('REDMINE_TICKET', 0, $new_ticket_id);
         $ticket_ref->dataCreate();
+        
+        // Create a history-entry with a reference to the ticket
+        $history = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.TestMan.TESTLAUF');
+        $history->setCellValue('TEST_CASE', 0, $input->getCellValue('TEST_CASE', 0));
+        $history->setCellValue('COMMENT', 0, $input->getCellValue('REDMINE_TICKET__SUBJECT', 0));
+        $history->setCellValue('TICKET', 0, $ticket_ref->getCellValue('UID', 0));
+        $history->setCellValue('TESTED_INSTALLATION', 0, '"'.$input->getCellValue('SYSTEM', 0).'"');
+        $history->dataCreate();
         
         $this->setResultDataSheet($result);
         $this->setResult('');
         $this->setResultMessage('New ticket <a target="_blank" href="' . $ticket_object->getDataConnection()->getUrl() . "issues/" . $new_ticket_id . '">#' . $new_ticket_id . '</a> created!');
         return;
+    }
+    
+    protected function buildTicketBody($template, $data){
+        if ($exception = $data->getCellValue('EXCEPTION', 0)) {
+            $exception = '
+					+*Exception:*+
+					<pre style="height: 150px; overflow: scroll;">' . $exception . '</pre>';
+        }
+        
+        if ($soll = $data->getCellValue('SOLL', 0)) {
+            $soll = '+*SOLL:*+
+					' . $soll;
+        }
+        
+        $today = date("d.m.Y");
+        
+        return <<<TEXT
++*Testnotiz:*+
+*AID:* {$data->getCellValue('TEST_CASE__FEATURE__MODULE__SHORT_NAME', 0)} {$data->getCellValue('TEST_CASE__FEATURE__MODULE__NAME', 0)}
+*Dialog/Menü:* {$data->getCellValue('TEST_CASE__FEATURE__MENU', 0)}
+*Testsystem:* {$data->getCellValue('SYSTEM', 0)}alexa, Klient {$data->getCellValue('MANDANT', 0)}, getestet in Version:
+{$today} {$data->getCellValue('VERSION', 0)}
+*DB-Nutzer/Schema:* {$data->getCellValue('DBSCHEMA', 0)}
+
++*Testablauf:*+
+{$data->getCellValue('TESTABLAUF', 0)}
+
+{$exception}
+
+{$soll}
+TEXT;
     }
 }
 ?>
