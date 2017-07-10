1c93727b5ecc167b187df7aad8cde4916fc93603
Andrej Kabachnik <andrej_ka@gmx.de>
2017-07-10 09:29:24 +0200

    NEW SqlSchemaInstaller

--- TestManApp.php
+++ TestManApp.php
@@ -1,7 +1,20 @@
 <?php
 namespace axenox\TestMan;
 
+use exface\Core\Interfaces\InstallerInterface;
+use exface\SqlDataConnector\SqlSchemaInstaller;
+
 class TestManApp extends \exface\Core\CommonLogic\AbstractApp
 {
+    public function getInstaller(InstallerInterface $injected_installer = null)
+    {
+        $installer = parent::getInstaller($injected_installer);
+        
+        $schema_installer = new SqlSchemaInstaller($this->getNameResolver());
+        $schema_installer->setDataConnection($this->getWorkbench()->model()->getObject('axenox.TestMan.FEATURE')->getDataConnection());
+        $installer->addInstaller($schema_installer);
+        
+        return $installer;
+    }
 }
 ?>
