50f66621663e9277d73f604dbf6235994cd64f60
Andrej Kabachnik <andrej_ka@gmx.de>
2018-11-22 17:41:07 +0100

    FIX installer error due to non-existent model

--- TestManApp.php
+++ TestManApp.php
@@ -4,6 +4,7 @@ namespace axenox\TestMan;
 use exface\Core\Interfaces\InstallerInterface;
 use exface\Core\CommonLogic\AppInstallers\SqlSchemaInstaller;
 use exface\Core\CommonLogic\Model\App;
+use exface\Core\Exceptions\Model\MetaObjectNotFoundError;
 
 class TestManApp extends App
 {
@@ -12,8 +13,12 @@ class TestManApp extends App
         $installer = parent::getInstaller($injected_installer);
         
         $schema_installer = new SqlSchemaInstaller($this->getSelector());
-        $schema_installer->setDataConnection($this->getWorkbench()->model()->getObject('axenox.TestMan.FEATURE')->getDataConnection());
-        $installer->addInstaller($schema_installer);
+        try {
+            $schema_installer->setDataConnection($this->getWorkbench()->model()->getObject('axenox.TestMan.FEATURE')->getDataConnection());
+            $installer->addInstaller($schema_installer);
+        } catch (MetaObjectNotFoundError $e) {
+            $this->getWorkbench()->getLogger()->warning('Cannot init SqlSchemInstaller for app ' . $this->getAliasWithNamespace . ': no model there yet!');
+        }
         
         return $installer;
     }
